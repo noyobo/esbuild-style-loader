06fc57ac57512d3de3c0a29ac5dcd39323772799
农有宝 <noyobo@gmail.com>
2023-05-26 20:59:00 +0800

    fix: css-loader resolve absolutePath (#6)

--- src/transform-less.ts
+++ src/transform-less.ts
@@ -5,6 +5,9 @@ export const transformLess = async (inputContext: string, filePath: string) => {
   return await lessEngine.render(inputContext, {
     filename: filePath,
     syncImport: true,
+    /**
+     * Legacy compatible
+     */
     plugins: [new NpmImportPlugin({ prefix: '~' })],
     sourceMap: {
       sourceMapFileInline: false,
