4cdcb8179fc87a2ff3537573b9111c2263c15fdb
noyobo <noyobo@gmail.com>
2023-06-06 19:46:08 +0800

    test: css-error

---
+++ __tests__/css-error/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+const App = () => (
+  <div className={'app'}>
+    <h1 className={'title'}>Hello World</h1>
+  </div>
+);
+
+createRoot(document.getElementById('root')).render(<App />);
