df92aa863513cb7c3063d6f15cd8f0c721a3b685
农有宝 <noyobo@gmail.com>
2023-06-06 19:57:33 +0800

    feat: catch transform css error (#9)

---
+++ __tests__/css-error/index.test.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { removeComments, runTest } from '../runTest';
+import fse from 'fs-extra';
+import { formatMessages } from 'esbuild';
+
+const OUTPUT_HTML = !!process.env.OUTPUT_HTML;
+
+describe(path.basename(path.dirname(__filename)), function () {
+  it('style-loader', async function () {
+    const output = path.resolve(__dirname, 'output');
+    fse.removeSync(output);
+    await runTest([path.resolve(__dirname, './index.jsx')], output).catch((error) => {
+      formatMessages(error.errors, { kind: 'error', color: false }).then((res) => {
+        expect(res.length).toBe(1);
+        expect(res[0]).toContain('[ERROR] Unexpected token Dimension');
+      });
+    });
+  });
+});
